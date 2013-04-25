4010fa612fd8ed6effe89fb2ec8d8dffd86a8cbb
Joshua Schwarz <jjs209@case.edu>
2013-04-24 23:16:58 -0400

    Josh palace with mickey as a gun with a crosshair and bullets implemented.
    Signed-off-by: Joshua Schwarz <jjs209@case.edu>

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -15,7 +15,7 @@ public class Bullet : MonoBehaviour {
 	
 	void OnTriggerEnter (Collider other)
     {
-		if(other.tag != "Gun" && other.tag != "Bullet")
+		if(other.name != "First Person Controller" && other.name != "Crosshair" && other.tag != "Bullet")
 		{
 			GameObject.Destroy(this.gameObject);
 			Debug.Log ("hit");
--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 using System.Collections;
 
 public class Gun : MonoBehaviour {
-
+	
+	
+	GameObject crosshair;
+	GameObject mickey;
 	// Use this for initialization
 	void Start () {
-
+		crosshair = GameObject.Find ("Crosshair");
+		crosshair.renderer.material.color = Color.red;
+		mickey = GameObject.Find ("Mickey");
 	}
 	
 	// Update is called once per frame
@@ -16,12 +21,20 @@ public class Gun : MonoBehaviour {
 			Quaternion rotation = new Quaternion(transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w);
 			Instantiate (Resources.Load ("Bullet"), position, rotation);
 		}	
+		//set up crosshair
+		crosshair.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+		crosshair.transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w);
+		crosshair.transform.Translate(new Vector3(0f,0f,5f));
+		//set up mickey
+		mickey.transform.position = new Vector3(transform.position.x, transform.position.y-.5f, transform.position.z);
+		mickey.transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w);
+		mickey.transform.Translate(new Vector3(0f,0f,.5f));
 		
 		GameObject[] bullets = GameObject.FindGameObjectsWithTag("Bullet");
 		foreach(GameObject bullet in bullets)
 		{
-			bullet.transform.Translate(new Vector3(0,0,.1f));
+			bullet.transform.Translate(new Vector3(0,0,1f));
 		}		
-		this.transform.Translate(new Vector3(Input.GetAxis ("Mouse X"), Input.GetAxis ("Mouse Y"), 0));
+		//this.transform.Translate(new Vector3(Input.GetAxis ("Mouse X"), Input.GetAxis ("Mouse Y"), 0));
 	}
 }
