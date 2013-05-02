39058864beaf1a283b9ef9d9dd788aa84eeb4d50
Joshua Schwarz <jjs209@case.edu>
2013-05-02 19:33:24 -0400

    Signed-off-by: Joshua Schwarz <jjs209@case.edu>

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -19,19 +19,21 @@ public class Gun : MonoBehaviour {
 		float x = mickey.transform.position.x;
 		float y = mickey.transform.position.y+1.9f;//bullets lined up with Mickey's head
 		float z = mickey.transform.position.z;
+		//shoot fire ball on left click
 		if(Input.GetMouseButtonDown(0))
 		{
 			Vector3 position = new Vector3(x,y,z);
 			Quaternion rotation = new Quaternion(transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w);
 			Instantiate (Resources.Load ("Bullet"), position, rotation);
 		}	
+		//shoot cannon ball on right click
 		if(Input.GetMouseButtonDown(1))
 		{
 			Vector3 position = new Vector3(x,y,z);
 			Quaternion rotation = new Quaternion(transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w);
 			Instantiate (Resources.Load ("Cannon Ball"), position, rotation);
 		}	
-		
+		//move all bullets forward
 		GameObject[] bullets = GameObject.FindGameObjectsWithTag("Bullet");
 		foreach(GameObject bullet in bullets)
 		{
